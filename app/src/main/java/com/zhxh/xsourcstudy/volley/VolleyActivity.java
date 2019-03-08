5a46bb280470bf2c3278d0a34d46c0618c695ea9
zhxhcoder <zhxhcoder@gmail.com>
2019-03-08 17:42:50 +0800

    源码注释

--- app/src/main/java/com/zhxh/xsourcstudy/volley/VolleyActivity.java
+++ app/src/main/java/com/zhxh/xsourcstudy/volley/VolleyActivity.java
@@ -3,6 +3,9 @@ package com.zhxh.xsourcstudy.volley;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -12,12 +15,18 @@ import com.android.volley.toolbox.Volley;
 import com.zhxh.xsourcstudy.R;
 
 public class VolleyActivity extends AppCompatActivity {
+    @BindView( R.id.title_text)
+    public TextView title_text;
+
     RequestQueue mQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_volley);
+        ButterKnife.bind(this);
+
+        title_text.setText("请求数据");
 
         mQueue = Volley.newRequestQueue(this);
 
@@ -28,7 +37,7 @@ public class VolleyActivity extends AppCompatActivity {
                 StringRequest mStringRequest = new StringRequest(Request.Method.GET, "https://www.baidu.com/", new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-
+                        title_text.setText(response.substring(0,20));
                     }
                 }, new Response.ErrorListener() {
                     @Override

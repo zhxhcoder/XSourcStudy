eba7caf3051b4fd527bd5dd8d377216070f23594
zhxhcoder <zhxhcoder@gmail.com>
2019-03-08 13:20:21 +0800

    OkHttp

--- app/src/main/java/com/zhxh/xsourcstudy/okhttp/OkhttpActivity.java
+++ app/src/main/java/com/zhxh/xsourcstudy/okhttp/OkhttpActivity.java
@@ -2,6 +2,7 @@ package com.zhxh.xsourcstudy.okhttp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import com.zhxh.xsourcstudy.R;
 
 public class OkhttpActivity extends AppCompatActivity {
@@ -10,5 +11,15 @@ public class OkhttpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_okhttp);
+
+        findViewById(R.id.btn_start).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
     }
 }

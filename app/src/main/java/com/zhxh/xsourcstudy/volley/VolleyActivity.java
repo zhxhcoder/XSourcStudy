7bfb7d148910e2629d7b7def7fa80cd57d66c910
zhxhcoder <zhxhcoder@gmail.com>
2019-03-08 12:05:20 +0800

    Volley测试用例

--- app/src/main/java/com/zhxh/xsourcstudy/volley/VolleyActivity.java
+++ app/src/main/java/com/zhxh/xsourcstudy/volley/VolleyActivity.java
@@ -2,15 +2,51 @@ package com.zhxh.xsourcstudy.volley;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.zhxh.xsourcstudy.R;
 
 public class VolleyActivity extends AppCompatActivity {
-    public static RequestQueue queues;
+    RequestQueue mQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_volley);
+
+        mQueue = Volley.newRequestQueue(this);
+
+
+        findViewById(R.id.btn_start).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                StringRequest mStringRequest = new StringRequest(Request.Method.GET, "https://www.baidu.com/", new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                });
+
+                mQueue.add(mStringRequest);
+            }
+        });
+
+        findViewById(R.id.btn_cancel).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mQueue.cancelAll("volley");
+            }
+        });
+
     }
 }

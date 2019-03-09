bb26a2a8d49a84001d6e565b7867946350272827
zhxhcoder <zhxhcoder@gmail.com>
2019-03-09 17:24:30 +0800

    源码学习入口

--- app/src/main/java/com/zhxh/xsourcstudy/MainActivity.kt
+++ app/src/main/java/com/zhxh/xsourcstudy/MainActivity.kt
@@ -3,6 +3,18 @@ package com.zhxh.xsourcstudy
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.zhxh.xsourcstudy.arouter.ARouterActivity
+import com.zhxh.xsourcstudy.butterknife.ButterknifeActivity
+import com.zhxh.xsourcstudy.dagger2.Dagger2Activity
+import com.zhxh.xsourcstudy.eventbus.EventbusActivity
+import com.zhxh.xsourcstudy.fresco.FrescoActivity
+import com.zhxh.xsourcstudy.glide.GlideActivity
+import com.zhxh.xsourcstudy.leakcanary.LeakCanaryActivity
+import com.zhxh.xsourcstudy.okhttp.OkhttpActivity
+import com.zhxh.xsourcstudy.retrofit.RetrofitActivity
+import com.zhxh.xsourcstudy.rxjava2.RxJava2Activity
+import com.zhxh.xsourcstudy.sophix.SophixActivity
+import com.zhxh.xsourcstudy.tinker.TinkerActivity
 import com.zhxh.xsourcstudy.volley.VolleyActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -17,6 +29,19 @@ class MainActivity : AppCompatActivity() {
 
 
         btn_volley.setOnClickListener { startActivity(Intent(this@MainActivity, VolleyActivity::class.java)) }
+        btn_retrofit.setOnClickListener { startActivity(Intent(this@MainActivity, RetrofitActivity::class.java)) }
+        btn_okhttp.setOnClickListener { startActivity(Intent(this@MainActivity, OkhttpActivity::class.java)) }
+        btn_rxjava.setOnClickListener { startActivity(Intent(this@MainActivity, RxJava2Activity::class.java)) }
+        btn_eventbus.setOnClickListener { startActivity(Intent(this@MainActivity, EventbusActivity::class.java)) }
+        btn_butterknife.setOnClickListener { startActivity(Intent(this@MainActivity, ButterknifeActivity::class.java)) }
+        btn_dagger.setOnClickListener { startActivity(Intent(this@MainActivity, Dagger2Activity::class.java)) }
+        btn_arouter.setOnClickListener { startActivity(Intent(this@MainActivity, ARouterActivity::class.java)) }
+        btn_leakcanary.setOnClickListener { startActivity(Intent(this@MainActivity, LeakCanaryActivity::class.java)) }
+        btn_tinker.setOnClickListener { startActivity(Intent(this@MainActivity, TinkerActivity::class.java)) }
+        btn_sophix.setOnClickListener { startActivity(Intent(this@MainActivity, SophixActivity::class.java)) }
+        btn_glide.setOnClickListener { startActivity(Intent(this@MainActivity, GlideActivity::class.java)) }
+        btn_fresco.setOnClickListener { startActivity(Intent(this@MainActivity, FrescoActivity::class.java)) }
+
     }
 
     /**
--- app/src/main/java/com/zhxh/xsourcstudy/fresco/FrescoActivity.java
+++ app/src/main/java/com/zhxh/xsourcstudy/fresco/FrescoActivity.java
@@ -1,14 +1,34 @@
 package com.zhxh.xsourcstudy.fresco;
 
+import android.content.Context;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.view.SimpleDraweeView;
 import com.zhxh.xsourcstudy.R;
 
 public class FrescoActivity extends AppCompatActivity {
+    //@BindView( R.id.simple_drawee_view)
+    //public SimpleDraweeView simple_drawee_view;
+    private final String imgUrl = "https://www.baidu.com/img/bd_logo1.png";
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(newBase);
+
+        Fresco.initialize(this);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fresco);
+
+
+        Uri uri = Uri.parse(imgUrl);
+        SimpleDraweeView draweeView = findViewById(R.id.simple_drawee_view);
+        draweeView.setImageURI(uri);
+
     }
 }

574f52350b09e37790211b5188778a4bf796d1c1
zhxhcoder <zhxhcoder@gmail.com>
2019-03-09 15:33:01 +0800

    Glide

--- app/src/main/java/com/zhxh/xsourcstudy/butterknife/ButterknifeActivity.java
+++ app/src/main/java/com/zhxh/xsourcstudy/butterknife/ButterknifeActivity.java
@@ -2,13 +2,19 @@ package com.zhxh.xsourcstudy.butterknife;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import com.zhxh.xsourcstudy.R;
 
 public class ButterknifeActivity extends AppCompatActivity {
-
+    @BindView( R.id.title_text)
+    public TextView title_text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_butterknife);
+
+        ButterKnife.bind(this);
     }
 }
--- app/src/main/java/com/zhxh/xsourcstudy/glide/GlideActivity.java
+++ app/src/main/java/com/zhxh/xsourcstudy/glide/GlideActivity.java
@@ -2,13 +2,21 @@ package com.zhxh.xsourcstudy.glide;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import com.zhxh.xsourcstudy.R;
 
 public class GlideActivity extends AppCompatActivity {
+    @BindView( R.id.iv_show)
+    public ImageView iv_show;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_glide);
+
+        ButterKnife.bind(this);
+
     }
 }

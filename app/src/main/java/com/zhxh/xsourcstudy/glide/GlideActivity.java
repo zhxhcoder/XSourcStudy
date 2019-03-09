1aef2c929c0579128feb027905e9f5dba2848611
zhxhcoder <zhxhcoder@gmail.com>
2019-03-09 15:48:13 +0800

    Glide4.0+

--- app/src/main/java/com/zhxh/xsourcstudy/glide/GlideActivity.java
+++ app/src/main/java/com/zhxh/xsourcstudy/glide/GlideActivity.java
@@ -1,15 +1,22 @@
 package com.zhxh.xsourcstudy.glide;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.RequestBuilder;
+import com.bumptech.glide.request.RequestOptions;
 import com.zhxh.xsourcstudy.R;
 
 public class GlideActivity extends AppCompatActivity {
-    @BindView( R.id.iv_show)
+    @BindView(R.id.iv_show)
     public ImageView iv_show;
+
+
+    private final String imgUrl = "https://www.baidu.com/img/bd_logo1.png";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +24,16 @@ public class GlideActivity extends AppCompatActivity {
         setContentView(R.layout.activity_glide);
 
         ButterKnife.bind(this);
+
+        RequestOptions myOptions = new RequestOptions()
+                .fitCenter()
+                .placeholder(R.mipmap.ic_red)
+                .override(100, 100);
+
+        Glide.with(this)
+                .load(imgUrl)
+                .apply(myOptions)
+                .into(iv_show);
 
     }
 }

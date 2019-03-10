678220fe270c6e34f4b5d9fd15450ec2e4a5b002
zhxhcoder <zhxhcoder@gmail.com>
2019-03-10 14:18:21 +0800

    eventbus源码分析

--- app/src/main/java/com/zhxh/xsourcstudy/eventbus/EventbusActivity.java
+++ app/src/main/java/com/zhxh/xsourcstudy/eventbus/EventbusActivity.java
@@ -2,13 +2,52 @@ package com.zhxh.xsourcstudy.eventbus;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import com.zhxh.xsourcstudy.R;
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
 
 public class EventbusActivity extends AppCompatActivity {
+    @BindView(R.id.tv_msg)
+    public TextView tv_msg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eventbus);
+
+        ButterKnife.bind(this);
+
+        findViewById(R.id.btn_register).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EventBus.getDefault().register(this);
+            }
+        });
+
+
+        findViewById(R.id.btn_unregister).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EventBus.getDefault().unregister(this);
+            }
+        });
+
+        findViewById(R.id.btn_post).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EventBus.getDefault().post(new MessageEvent("测试数据"));
+            }
+        });
     }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onMessageEvent(MessageEvent event) {
+        tv_msg.setText(event.getMsg());
+    }
+
 }
---
+++ app/src/main/java/com/zhxh/xsourcstudy/eventbus/MessageEvent.java
@@ -0,0 +1,18 @@
+package com.zhxh.xsourcstudy.eventbus;
+
+public class MessageEvent {
+
+    private String msg;
+
+    public MessageEvent(String msg) {
+        this.msg = msg;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}

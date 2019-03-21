b2072728b98cdd62dce1d6c1484c5eb973b545db
zhxhcoder <zhxhcoder@gmail.com>
2019-03-21 14:52:53 +0800

    测试

--- libmonitor/src/main/java/com/zhxh/libmonitor/TestActivity.java
+++ libmonitor/src/main/java/com/zhxh/libmonitor/TestActivity.java
@@ -8,8 +8,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class TestActivity extends AppCompatActivity {
+    
     private EditText edtvTest = null;
     private Button btnTest = null;
     private String info = "";
@@ -17,11 +17,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_test);
         if (savedInstanceState != null) {
-            System.out.println("--------main---------> text: " + savedInstanceState.getString("text"));
+            System.out.println("--------test---------> text: " + savedInstanceState.getString("text"));
         }
-        System.out.println("--------main---------> onCreate");
+        System.out.println("--------test---------> onCreate");
         edtvTest = findViewById(R.id.edtv_test);
         btnTest = findViewById(R.id.btn_test);
         btnTest.setOnClickListener(listener);
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
             switch (v.getId()) {
                 case R.id.btn_test:
                     Intent it1 = new Intent();
-                    it1.setClass(MainActivity.this, TestActivity.class);
+                    it1.setClass(TestActivity.this, MainActivity.class);
                     startActivity(it1);
                     break;
                 default:
@@ -48,56 +48,57 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         // TODO Auto-generated method stub
         super.onStart();
-        System.out.println("--------main---------> onStart");
+        System.out.println("--------test---------> onStart");
     }
 
     @Override
     protected void onRestart() {
         // TODO Auto-generated method stub
         super.onRestart();
-        System.out.println("--------main---------> onRestart");
+        System.out.println("--------test---------> onRestart");
     }
 
     @Override
     protected void onResume() {
         // TODO Auto-generated method stub
         super.onResume();
-        System.out.println("--------main---------> onResume");
+        System.out.println("--------test---------> onResume");
     }
 
     @Override
     protected void onPause() {
         // TODO Auto-generated method stub
         super.onPause();
-        System.out.println("--------main---------> onPause");
+        System.out.println("--------test---------> onPause");
     }
 
     @Override
     protected void onStop() {
         // TODO Auto-generated method stub
         super.onStop();
-        System.out.println("--------main---------> onStop");
+        System.out.println("--------test---------> onStop");
     }
 
     @Override
     protected void onDestroy() {
         // TODO Auto-generated method stub
         super.onDestroy();
-        System.out.println("--------main---------> onDestroy");
+        System.out.println("--------test---------> onDestroy");
     }
 
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
         super.onSaveInstanceState(savedInstanceState);
         savedInstanceState.putString("text", edtvTest.getText().toString());
-        System.out.println("--------main---------> onSaveInstanceState");
+        System.out.println("--------test---------> onSaveInstanceState");
     }
 
     @Override
     public void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
         info = savedInstanceState.getString("text");
-        System.out.println("--------main---------> onRestoreInstanceState");
+        System.out.println("--------test---------> onRestoreInstanceState");
 
     }
+
 }

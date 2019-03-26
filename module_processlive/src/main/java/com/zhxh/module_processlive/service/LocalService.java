b9ab8b45ea792c73ee334bfaa439f2d886ffa893
zhxhcoder <zhxhcoder@gmail.com>
2019-03-26 18:57:08 +0800

    keep

---
+++ module_processlive/src/main/java/com/zhxh/module_processlive/service/LocalService.java
@@ -0,0 +1,16 @@
+package com.zhxh.module_processlive.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class LocalService extends Service {
+    public LocalService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}

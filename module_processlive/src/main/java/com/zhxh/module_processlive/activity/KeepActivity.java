78ad6f2384578dd5efc4798e00f6fc6387680633
zhxhcoder <zhxhcoder@gmail.com>
2019-03-26 12:46:42 +0800

    keep

---
+++ module_processlive/src/main/java/com/zhxh/module_processlive/activity/KeepActivity.java
@@ -0,0 +1,14 @@
+package com.zhxh.module_processlive.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.zhxh.module_processlive.R;
+
+public class KeepActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_keep);
+    }
+}
---
+++ module_processlive/src/main/java/com/zhxh/module_processlive/activity/KeepManager.java
@@ -0,0 +1,24 @@
+package com.zhxh.module_processlive.activity;
+
+/**
+ * Created by zhxh on 2019/3/26
+ */
+public class KeepManager {
+
+    private volatile static KeepManager instance;
+
+    private KeepManager() {
+    }
+
+    public static KeepManager getInstance() {
+        if (instance == null) {
+            synchronized (KeepManager.class) {
+                if (instance == null) {
+                    instance = new KeepManager();
+                }
+            }
+        }
+
+        return instance;
+    }
+}

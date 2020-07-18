d07886bf077a0115702fd91382bea4ca25d79f1f
zhxh <xiaohuizhu4@creditease.cn>
2020-07-18 12:05:58 +0800

    不同启动模式，finish()与否的生命周期

---
+++ app/src/main/java/com/zhxh/xsourcstudy/lifecycle/ActivityB.java
@@ -0,0 +1,76 @@
+package com.zhxh.xsourcstudy.lifecycle;
+
+import android.content.Intent;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.zhxh.xsourcstudy.R;
+
+public class ActivityB extends AppCompatActivity {
+
+    private static final String TAG = "ActivityB";
+    TextView goBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_b_layout);
+        Log.d(TAG, "onCreate");
+
+        goBtn = findViewById(R.id.goBtn);
+        goBtn.setOnClickListener(v -> startActivity(new Intent(ActivityB.this, ActivityC.class)));
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        Log.d(TAG, "onSaveInstanceState");
+
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onRestoreInstanceState(savedInstanceState, persistentState);
+        Log.d(TAG, "onRestoreInstanceState");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+}
